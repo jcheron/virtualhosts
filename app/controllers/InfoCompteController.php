4001ed00f6af97ace7056d793c7dfcadbf0770d6
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2016-12-01 11:10:24 +0100

    ModfiInfo v2.0

--- app/controllers/InfoCompteController.php
+++ app/controllers/InfoCompteController.php
@@ -1,4 +1,5 @@
 <?php
+use Phalcon\Mvc\View;
 class InfoCompteController extends ControllerBase{
 	public function indexAction(){
 		
@@ -9,10 +10,14 @@ class InfoCompteController extends ControllerBase{
 	}
 	public function ModifInfoAction(){
 		$semantic=$this->semantic;
-		$this->secondaryMenu($this->controller,$this->action);
-		$this->tools($this->controller,$this->action);
-		
+		$user=User::findFirst();
+		$this->loadMenus();
+		$message=$semantic->htmlMessage("messageInfo","<b> Bienvenue ".$user->getName());
+		$message2=$semantic->htmlMessage("messageInfomodif","Votre Login :   <input type='textarea' placeholder='".$user->getLogin()."'><br></br> Votre mot de passe :  <input type='password' placeholder='".$user->getPassword()."'><br></br> Votre nom :  <input type='textarea' placeholder='".$user->getName()."'><br></br> Votre e-mail :  <input type='textarea' placeholder='".$user->getEmail()."'");
+		$button=$semantic->htmlButton("btValider","Validez")->setColor("green");
+		$button->onClick();
 		$this->jquery->compile($this->view);
+		
 		
 		
 	}
--- app/views/InfoCompte/ModifInfo.volt
+++ app/views/InfoCompte/ModifInfo.volt
@@ -1,1 +1,6 @@
 <h1>Attention vous entrez dans une zone de modification !</h1>
+{{ q["messageInfo"] }}
+Informations a modifiées :
+{{ q["messageInfomodif"] }}
+<br></br>
+{{ q["btValider"] }}
